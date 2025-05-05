7cb00eb5beef6454ef8cc2d99401ab0e7d1afcd9
RTB456 <159496952+RTB456@users.noreply.github.com>
2025-05-05 16:24:42 -0500

    added angles to utils2.py

--- utils2.py
+++ utils2.py
@@ -1,6 +1,76 @@
 import numpy as np
 
 
+#Function to compute the angle between 3 points
+def compute_angle(p1, p2, p3):
+    """
+    Computes the angle (in degrees) between vectors (P1 -> P2) and (P3 -> P2).
+  
+    Parameters:
+    - P1, P2, P3: 3D coordinates of the points forming the angle.
+  
+    Returns:
+   - Angle in degrees.
+    """
+    vec1 = p1 - p2
+    vec2 = p3 - p2
+
+    # Normalize vectors
+    vec1_norm = np.linalg.norm(vec1)
+    vec2_norm = np.linalg.norm(vec2)
+  
+    if vec1_norm < 1e-6 or vec2_norm < 1e-6:
+       return 0  # Return 0 for degenerate cases
+      
+    vec1 = vec1 / vec1_norm
+    vec2 = vec2 / vec2_norm
+
+
+   # Compute angle using dot product
+    dot_product = np.dot(vec1, vec2)
+    angle_rad = np.arccos(np.clip(dot_product, -1.0, 1.0))  # Clip for stability
+    return np.degrees(angle_rad)
+
+def extract_angles_from_frame(frame_idx, pose3d_data):
+    """
+    Compute joint angles at frame frame_idx from pose3d_data.
+
+    Parameters:
+    -frame_idx(int): Index of the frame to compute angles for.
+    -pose3d_data (np.ndarray): shape (num_frames, 17, 3)
+
+    Returns: 
+    - angles_dict (dict): keys are (joint_a, vertex_joint, joint_b), values are angles in degrees. 
+    """
+    joint_pairs = [[0, 1], [1, 3], [0, 2], [2, 4],
+                   [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
+                   [5, 11], [6, 12], [11, 12],
+                   [11, 13], [12, 14], [13, 15], [14, 16]]
+    
+    from collections import defaultdict 
+    joint_3d = pose3d_data[frame_idx] # shape (17,3)
+    
+    #build adjacent list
+    neighbors = defaultdict(list)
+    for a, b in joint_pairs:
+        neighbors[a].append(b)
+        neighbors[b].append(a)
+    
+    angles_dict = {}
+    for center_joint, connected in neighbors.items():
+        if len(connected) < 2:
+            continue
+        for i in range(len(connected)):
+            for j in range(i +1, len(connected)):
+                u, v = connected[i], connected[j]
+                p1, p2, p3 = joint_3d[u], joint_3d[center_joint], joint_3d[v]
+                angle = compute_angle(p1, p2, p3)
+                key = tuple(sorted([u, center_joint, v]))  # e.g., (4, 6, 12)
+                angles_dict[key] = angle
+
+    return angles_dict    
+
+
 def get_min_angle_diff(selected_angles, target_angles, search_width):
 
     '''
