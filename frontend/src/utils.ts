ae8f2b13a92b12efd2763cd4184d441a3e40871e
Max Glass <glassmax13@gmail.com>
2025-05-09 15:28:50 -0500

    begin frontend

---
+++ frontend/src/utils.ts
@@ -0,0 +1,39 @@
+import squatImage from "./images/squat.png";
+
+export const references = [
+  {
+    imageSrc: squatImage, // Replace with your actual image paths
+    altText: "Man squatting",
+    description: "Squat",
+  },
+  //   {
+  //     imageSrc: "/images/image2.png", // Replace with your actual image paths
+  //     altText: "Description of image 2",
+  //     description:
+  //       "Here is some information about the second image. It could be longer or shorter.",
+  //   },
+  //   {
+  //     imageSrc: "/images/image3.gif", // Replace with your actual image paths
+  //     altText: "Description of image 3",
+  //     description:
+  //       "The third image has its own unique description here. Make sure it is informative.",
+  //   },
+  //   {
+  //     imageSrc: "/images/image4.jpeg", // Replace with your actual image paths
+  //     altText: "Description of image 4",
+  //     description:
+  //       "Another item in our grid, this is the fourth image and its accompanying text.",
+  //   },
+  //   {
+  //     imageSrc: "/images/image5.svg", // Replace with your actual image paths
+  //     altText: "Description of image 5",
+  //     description:
+  //       "This is the fifth image. SVG images are also supported if you configure them correctly.",
+  //   },
+  //   {
+  //     imageSrc: "/images/image6.webp", // Replace with your actual image paths
+  //     altText: "Description of image 6",
+  //     description:
+  //       "Last but not least, the sixth image in this demonstration grid with its description below.",
+  //   },
+];
